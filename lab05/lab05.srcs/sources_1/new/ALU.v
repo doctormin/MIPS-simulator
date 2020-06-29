2ec824262b2d96880789537775cea0b5c61781f2
YiminZhao <doctormin@sjtu.edu.cn>
2020-06-29 23:03:17 +0800

    added : ALU decoder & ISA.v

--- lab05/lab05.srcs/sources_1/new/ALU.v
+++ lab05/lab05.srcs/sources_1/new/ALU.v
@@ -28,11 +28,12 @@ module ALU #(
                 //input
                 input  signed [DATA_WIDTH-1:0] i_oprand1,
                 input  signed [DATA_WIDTH-1:0] i_oprand2,
-                input  [3:0] i_alu_ctr_out,
+                input  [3:0] i_alu_ctr,
                 //output
                 output reg o_zero,
                 output reg [DATA_WIDTH-1:0] o_alu_res
             );
+
 localparam ALU_ADD   = 1,
            ALU_ADDU  = 2,
            ALU_SUB   = 3,
@@ -42,103 +43,211 @@ localparam ALU_ADD   = 1,
            ALU_XOR   = 7,
            ALU_NOR   = 8,
            ALU_SLL   = 9,
-           ALU_SLLV  = 10,
-           ALU_SRL   = 11,
-           ALU_SRLV  = 12,
-           ALU_SRA   = 13,
-           ALU_SRAV  = 14,
-           ALU_SLT   = 15,
-           ALU_SLTIU = 16,
-           ALU_SLTU  = 17,
-           ALU_LUI   = 18;
-
+           ALU_SRL   = 10,
+           ALU_SRA   = 11,
+           ALU_SLT   = 12,
+           ALU_SLTU  = 13,
+           ALU_SLE   = 14,
+           ALU_LUI   = 15,
+           ALU_NO    = 16;
 
 always @(*) begin
-    o_alu_res <= {DATA_WIDTH{1'b0}};
-    o_zero <= 1'bx;
-    case(i_alu_ctr_out)
+    o_alu_res = {DATA_WIDTH{1'b0}};
+    o_zero = 1'bx;
+    case(i_alu_ctr)
+        /*
+           ADD  : rd = rs + rt  (with overflow exception)
+           ADDI : rt = rs + imm (with overflow exception)
+           SB   : MEM[rs + imm] <- rt[7 :0]
+           SH   : MEM[rs + imm] <- rt[15:0]
+           SW   : MEM[rs + imm] <- rt[31:0]
+           LB   : rt            <- sign_extend(MEM[rs + imm])
+           LBU  : rt            <- zero_extend(MEM[rs + imm])
+           LH   : rt            <- sign_extend(MEM[rs + imm])
+           LHU  : rt            <- zero_extend(MEM[rs + imm])
+           LW   : rt            <- MEM[rs + imm]
+        */
         ALU_ADD:  begin
-            o_alu_res <= i_oprand1 + i_oprand2;
+            o_alu_res = i_oprand1 + i_oprand2;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
         
+        /* 
+           ADDU :  rd = rs + rt  (without overflow exception)
+           ADDIU:  rt = rs + imm (without overflow exception)
+        */
         ALU_ADDU: begin
-            o_alu_res <= i_oprand1 + i_oprand2;
+            o_alu_res = i_oprand1 + i_oprand2;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        /*
+           SUB  : rd = rs - rt (with overflow exception)
+        */
         ALU_SUB:  begin
-            o_alu_res <= i_oprand1 - i_oprand2;
+            o_alu_res = i_oprand1 - i_oprand2;
             if(o_alu_res == {DATA_WIDTH{1'b0}})
-                o_zero <= 1'b1;
+                o_zero = 1'b1;
             else
-                o_zero <= 1'bx;
+                o_zero = 1'b0;
         end
 
+        /*
+           SUBU  : rd = rs - rt (without overflow exception)
+           BEQ / BNE  : ?  = rs - rt (without overflow exception)
+        */
         ALU_SUBU: begin
-            o_alu_res <= i_oprand1 - i_oprand2;
+            o_alu_res = i_oprand1 - i_oprand2;
             if(o_alu_res == {DATA_WIDTH{1'b0}})
-                o_zero <= 1'b1;
+                o_zero = 1'b1;
             else
-                o_zero <= 1'bx;
+                o_zero = 1'b0;
         end
 
+        /*
+           AND  : rd = rs & rt
+           ANDI : rt = rs & (zero_extend)imm
+        */
         ALU_AND:  begin
-            o_alu_res <= i_oprand1 & i_oprand2;
+            o_alu_res = i_oprand1 & i_oprand2;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        /*
+           OR   : rd = rs | rt
+           ORI  : rt = rs | (zero_extend)imm
+        */
         ALU_OR:   begin
-            o_alu_res <= i_oprand1 | i_oprand2;
+            o_alu_res = i_oprand1 | i_oprand2;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        /*
+           XOR  : rd = rs ^ rt
+           XORI : rt = rs ^ (zero_extend)imm
+        */
         ALU_XOR:  begin
-            o_alu_res <= i_oprand1 ^ i_oprand2;
+            o_alu_res = i_oprand1 ^ i_oprand2;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        // NOR  : rd = ~(rs | rt)
         ALU_NOR:  begin
-            o_alu_res <= ~(i_oprand1 | i_oprand2);
-        end
-        
-        ALU_SLL:  begin //TODO: SLL mux
-            o_alu_res <= i_oprand2 << i_oprand2;
-        end
-
-        ALU_SLL:  begin
-            o_alu_res <= i_oprand2 << i_oprand1[4:0];
+            o_alu_res = ~(i_oprand1 | i_oprand2);
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
-        ALU_SRL:  begin //TODO: SRL mux
-            o_alu_res <= i_oprand2 >> i_oprand2;
+        //逻辑左移 rd = rt << rs || rd = rt << sa
+        ALU_SLL:  begin 
+            o_alu_res = i_oprand2 << $unsigned(i_oprand1);
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
-        ALU_SRLV: begin
-            o_alu_res <= i_oprand2 >> i_oprand1[4:0];
+        //逻辑右移 rd = rt >> rs || rd = rt >> sa
+        ALU_SRL:  begin 
+            o_alu_res = i_oprand2 >> $unsigned(i_oprand1);
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
-        ALU_SRA:  begin //TODO: SRA mux
-            o_alu_res <= i_oprand2 >>> i_oprand2;
+        //算数右移  rd = rt >>> rs || rd = rt >>> sa
+        ALU_SRA:  begin 
+            o_alu_res = i_oprand2 >>> $unsigned(i_oprand1);
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
-        ALU_SRAV: begin
-            o_alu_res <= i_oprand2 >>> i_oprand1[4:0];
-        end
-
+        /*
+           SLT  : rd = (rs < rt)
+           SLTI : rt = (rs < imm)
+           BGEZ / BGTZ / BGEZAL / BLEZ / BLTZ / BLTZAL : ?  = rs ? 0 (without overflow exception)
+        */
         ALU_SLT:  begin
-            if(i_oprand1 < i_oprand2)
-                o_alu_res <= 1;
+            if($signed(i_oprand1) < $signed(i_oprand2))
+                o_alu_res = 1;
             else
-                o_alu_res <= 0;
+                o_alu_res = 0;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
-        ALU_SLTIU: begin
-            if(i_oprand1 < $unsigned(i_oprand2))
-                o_alu_res <= 1;
+        /*
+           SLTU : rd = ($unsigned(rs) < $unsigned(rt))
+           SLTIU: rt = ($unsigned(rs) < $unsigned(imm))
+        */
+        ALU_SLTU: begin
+            if($unsigned(i_oprand1) < $unsigned(i_oprand2))
+                o_alu_res = 1;
             else
-                o_alu_res <= 0;
+                o_alu_res = 0;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        /*
+           BGTZ
+           BLEZ
+        */
+        ALU_SLE: begin
+            if($signed(i_oprand1) <= $signed(i_oprand2))
+                o_alu_res = 1;
+            else
+                o_alu_res = 0;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
+        end
+
+
+        // LUI  : rt = imm << 16
         ALU_LUI:  begin
-            o_alu_res <= (i_oprand2 << 16);
+            o_alu_res = (i_oprand2 << 16);
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
         end
 
+        /*
+           JR
+         */
+        ALU_NO:  begin
+            o_alu_res = 32'bx;
+            if(o_alu_res == {DATA_WIDTH{1'b0}})
+                o_zero = 1'b1;
+            else
+                o_zero = 1'b0;
+        end
         default: begin
 			$display("%x:illegal ALU ctl code %b\n", 0, i_ALUCTL);
 		end
